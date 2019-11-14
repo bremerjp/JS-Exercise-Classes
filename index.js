0c327c5f988fea4d6f2e4a6d12b2e24f005b3627
Jacob Bremer <bremerjp@gmail.com>
2019-11-14 15:53:57 -0500

    completed tasks 2-3

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ class Airplane {
 class Person {
   constructor(name, age) {
     this.name = name,
-    this.age = age,
-    this.stomach = []
+      this.age = age,
+      this.stomach = []
   }
   eat(food) {
     if (this.stomach.length < 10) {
@@ -74,7 +74,26 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance) {
+    this.odometer += distance;
+    this.tank -= distance / this.milesPerGallon;
 
+    let milesLeftUntilEmpty = this.tank * this.milesPerGallon;
+    if (distance > milesLeftUntilEmpty) {
+      this.odometer += milesLeftUntilEmpty;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+  }
 }
 
 /*
@@ -90,7 +109,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(attributes) {
+    this.name = attributes.name,
+    this.age = attributes.age,
+    this.location = attributes.location
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`
+  }
 }
 
 /*
@@ -108,7 +134,7 @@ class Lambdasian {
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
 class Instructor {
-
+  
 }
 
 /*
